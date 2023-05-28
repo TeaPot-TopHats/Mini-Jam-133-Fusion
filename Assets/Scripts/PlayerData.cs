341e4d152aab177290ab3f2200b56f961cfbe23e
holypeachy <frankberm3@gmail.com>
2023-05-28 17:15:04 -0600

    Added the rest of the buttons. Added the charge mechanic. Can now add and remove charges, can cycle through charges. Added a cursor and limits to it. Added ChargeType enum. Added primary attack. Added barebones JumpDimension method to PlayerData.

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -5,5 +5,218 @@ using UnityEngine;
 public class PlayerData : MonoBehaviour
 {
 	public int Health = 10;
-	public float MoveSpeed = 4.5f; 
+	public int Attack = 5;
+	public int Defense = 5;
+	
+	public float MoveSpeed = 4.5f;
+	
+	public const int MAX_CHARGES = 3;
+	public ChargeType SelectedCharge = ChargeType.FIRE;
+	
+	public int FireCharge = 0;
+	public int IceCharge = 0;
+	public int ElectricCharge = 0;
+	
+
+	private SpriteRenderer Sprite;
+	private Rigidbody2D Rigid;
+	private Animator Anim;
+
+	private PlayerInputHandler InputH;
+	[SerializeField] private GameObject WeaponObject;
+
+
+	private SpriteRenderer WeaponSprite;
+
+	private void Start()
+	{
+		Sprite = GetComponent<SpriteRenderer>();
+		Rigid = GetComponent<Rigidbody2D>();
+		// Anim = GetComponent<Animator>();
+		InputH = GetComponent<PlayerInputHandler>();
+		WeaponSprite = WeaponObject.GetComponent<SpriteRenderer>();
+	}
+
+	private void FixedUpdate()
+	{
+		SpriteFlipCheck();
+		// MovementAnimationCheck();
+	}
+
+	private void SpriteFlipCheck()
+	{
+		if (InputH.AimVector.x < 0)
+		{
+			Sprite.flipX = false;
+			WeaponSprite.flipX = false;
+			// WeaponSprite.flipY = true;
+		}
+		else if (InputH.AimVector.x > 0)
+		{
+			Sprite.flipX = true;
+			WeaponSprite.flipX = true;
+			// WeaponSprite.flipY = false;
+
+		}
+	}
+
+
+	// private void MovementAnimationCheck()
+	// {
+	//     if (InputH.Movement == Vector2.zero)
+	//     {
+	//         Anim.SetBool("IsMoving", false);
+	//     }
+	//     else if ((InputH.Movement.y != 0 && Rigid.velocity.y == 0) && (Mathf.Abs(Rigid.velocity.x) != 0f))
+	//     {
+	//         Anim.SetBool("IsMoving", true);
+	//     }
+	//     else if ((InputH.Movement.y != 0 && Rigid.velocity.y == 0) && (Mathf.Abs(Rigid.velocity.x) == 0f))
+	//     {
+	//         Anim.SetBool("IsMoving", false);
+	//     }
+	//     else if ((InputH.Movement.x != 0 && Rigid.velocity.x == 0) && (Mathf.Abs(Rigid.velocity.y) != 0f))
+	//     {
+	//         Anim.SetBool("IsMoving", true);
+	//     }
+	//     else if ((InputH.Movement.x != 0 && Rigid.velocity.x == 0) && (Mathf.Abs(Rigid.velocity.y) == 0f))
+	//     {
+	//         Anim.SetBool("IsMoving", false);
+	//     }
+	//     else
+	//     {
+	//         Anim.SetBool("IsMoving", true);
+	//     }
+	// }
+
+	public void TakeDamage()
+	{
+		// here
+		CheckDeath();
+	}
+	
+	private void CheckDeath()
+	{
+		
+	}
+	
+	public void AddCharge(ChargeType type)
+	{
+		if(type == ChargeType.FIRE)
+		{
+			if(FireCharge != MAX_CHARGES)
+			{
+				FireCharge++;
+			}
+		}
+		else if (type == ChargeType.ICE)
+		{
+			if (IceCharge != MAX_CHARGES)
+			{
+				IceCharge++;
+			}
+		}
+		else if (type == ChargeType.ELECTRIC)
+		{
+			if (ElectricCharge != MAX_CHARGES)
+			{
+				ElectricCharge++;
+			}
+		}
+	}
+	
+	public void RemoveCharge(ChargeType type)
+	{
+		if (type == ChargeType.FIRE)
+		{
+			if (FireCharge > 0)
+			{
+				FireCharge--;
+			}
+		}
+		else if (type == ChargeType.ICE)
+		{
+			if (IceCharge > 0)
+			{
+				IceCharge--;
+			}
+		}
+		else if (type == ChargeType.ELECTRIC)
+		{
+			if (ElectricCharge > 0)
+			{
+				ElectricCharge--;
+			}
+		}
+	}
+	
+	// 1 means up -1 means down on the wheel
+	public void CycleChargeType(int value)
+	{
+		if(SelectedCharge == ChargeType.FIRE)
+		{
+			if(value == 1)
+			{
+				SelectedCharge = ChargeType.ELECTRIC;
+			}
+			else if(value == -1)
+			{
+				SelectedCharge = ChargeType.ICE;
+			}
+		}
+		else if (SelectedCharge == ChargeType.ICE)
+		{
+			if (value == 1)
+			{
+				SelectedCharge = ChargeType.FIRE;
+			}
+			else if (value == -1)
+			{
+				SelectedCharge = ChargeType.ELECTRIC;
+			}
+		}
+		else if (SelectedCharge == ChargeType.ELECTRIC)
+		{
+			if (value == 1)
+			{
+				SelectedCharge = ChargeType.ICE;
+			}
+			else if (value == -1)
+			{
+				SelectedCharge = ChargeType.FIRE;
+			}
+		}
+	}
+	
+	public void JumpDimension()
+	{
+		if (SelectedCharge == ChargeType.FIRE && FireCharge == MAX_CHARGES)
+		{
+			for (int i = 0; i < MAX_CHARGES; i++)
+			{
+				RemoveCharge(ChargeType.FIRE);
+			}
+		}
+		else if (SelectedCharge == ChargeType.ICE && IceCharge == MAX_CHARGES)
+		{
+			for (int i = 0; i < MAX_CHARGES; i++)
+			{
+				RemoveCharge(ChargeType.ICE);
+			}
+		}
+		else if (SelectedCharge == ChargeType.ELECTRIC && ElectricCharge == MAX_CHARGES)
+		{
+			for (int i = 0; i < MAX_CHARGES; i++)
+			{
+				RemoveCharge(ChargeType.ELECTRIC);
+			}
+		}
+		else
+		{
+			Debug.LogError("JumpDimension");
+		}
+	}
+	
+	
+
 }
--- Assets/Scripts/PlayerInputHandler.cs
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -8,27 +8,141 @@ public class PlayerInputHandler : MonoBehaviour
 	// General components needed
 	[SerializeField] private Camera MainCamera; // Needed to convert the position of the mouse relative to the camera, to world coordinates
 	[SerializeField] private PlayerInput PlayerInputComponent; // Component made by the "new input system"
+	
+	private Rigidbody2D Rigid;
+	private PlayerData Data;
 
 	// Movement
 	[SerializeField] public Vector2 Movement;
 
 	// Aiming
 	public Vector2 AimCoord; // Stores the world location where the mouse or controller is pointing to
-	public Vector2 AimVector;// {get; private set;} // Stores the vector that points to where we are aiming
-	public float AimAngle; //{get; private set;} // We convert the AimVector to location and we put the angle where we are aiming here in degrees
+	public Vector2 AimVector;// Stores the vector that points to where we are aiming
+	public float AimAngle;  // We convert the AimVector to location and we put the angle where we are aiming here in degrees
+	// private Quaternion rotation;
+	
+	[SerializeField] private GameObject WeaponObject;
+	
+	[SerializeField] private GameObject Cursor;
+	private Transform CursorTransform;
+	[SerializeField] private GameObject RealCursor;
+	
+	[SerializeField] private float cursorLimitX = 2f;
+	[SerializeField] private float cursorLimitY = 2f;
+	
+
+	// Control flow
+	public bool canMove = true;
+	public bool canLook = true;
+	public bool canAttack = true;
+	
+	public bool isDashing = false;
+	
+	public bool CursorLimit = true;
+
+
+	// Combat
+	private Vector3 WeaponVector;
+	[SerializeField] private LayerMask EnemyLayer;
+	
+	Vector2 enemyToPlayerVector;
+	float dotProduct;
+	float absoluteMagnitude;
+	float angleBetween;
+	float aimRange;
+	float angleInterval;
+	float currentAngle;
+	Collider2D[] enemyColliders;
+	
+	public float MeleeRange = 30f;
+	public float MeleeReach = 2f;
+	public float AttackCooldown = 1f;
+
+	// Debug
+	bool drawMeleeGizmos = false;
 
 
 	private void Start()
 	{
+		Rigid = GetComponent<Rigidbody2D>();
+		Data = GetComponent<PlayerData>();
+		CursorTransform = Cursor.GetComponent<Transform>();
+	}
+	
+	private void Update()
+	{
+
+	}
+
+	private void FixedUpdate()
+	{
+		// Movement
+		if (isDashing)
+		{
+			return;
+		}
 		
+		if (canMove)
+		{
+			Vector2 targetSpeed = new Vector2(Movement.x * Data.MoveSpeed, Movement.y * Data.MoveSpeed);
+			Vector2 speedDif = targetSpeed - Rigid.velocity;
+			Vector2 actualSpeed = speedDif * new Vector2(12, 12); // Change the vector values to change acceleration
+			Rigid.AddForce(actualSpeed);
+		}
+		else
+		{
+			Rigid.velocity = Vector2.zero;
+		}
+
+
 	}
 
 
 	public void OnLook(InputAction.CallbackContext context)
 	{
-		AimCoord = MainCamera.ScreenToWorldPoint(context.ReadValue<Vector2>());
-		AimVector = AimCoord - (Vector2)transform.position;
-		AimAngle = Mathf.Atan2(AimVector.y, AimVector.x) * Mathf.Rad2Deg;
+		if(canLook)
+		{
+			AimCoord = MainCamera.ScreenToWorldPoint(context.ReadValue<Vector2>());
+			AimVector = AimCoord - (Vector2)transform.position;
+			AimAngle = Mathf.Atan2(AimVector.y, AimVector.x) * Mathf.Rad2Deg;
+
+			// Weapon Rotation
+			// rotation = Quaternion.AngleAxis(AimAngle, Vector3.forward);
+			// WeaponObject.transform.rotation = rotation;
+
+			// Cursor
+			if(CursorLimit)
+			{
+				if (AimVector.x < -cursorLimitX || AimVector.x > cursorLimitX)
+				{
+					if (AimVector.y < -cursorLimitY || AimVector.y > cursorLimitY)
+					{
+						CursorTransform.localPosition = new Vector2(CursorTransform.localPosition.x, CursorTransform.localPosition.y);
+					}
+					else
+					{
+						CursorTransform.localPosition = new Vector2(CursorTransform.localPosition.x, AimVector.y);
+					}
+				}
+				else
+				{
+					if (AimVector.y < -cursorLimitY || AimVector.y > cursorLimitY)
+					{
+						CursorTransform.localPosition = new Vector2(AimVector.x, CursorTransform.localPosition.y);
+					}
+					else
+					{
+						CursorTransform.localPosition = new Vector2(AimVector.x, AimVector.y);
+					}
+				}	
+			}
+			else
+			{
+				CursorTransform.localPosition = AimVector;
+			}
+		}
+		
+		
 	}
 
 
@@ -40,17 +154,146 @@ public class PlayerInputHandler : MonoBehaviour
 
 	public void OnFire(InputAction.CallbackContext context)
 	{
-		Debug.Log("Fire!");
-	}
+		if(canAttack && context.started)
+		{
+			// Debug
+			Debug.Log("BFA: Melee");
+			drawMeleeGizmos = true;
 
+			WeaponVector = transform.position;
+
+			// We get all the enemyies we have hit
+			enemyColliders = Physics2D.OverlapCircleAll(WeaponVector, MeleeReach, EnemyLayer);
+
+			foreach (Collider2D enemy in enemyColliders)
+			{
+				Debug.Log("BFA: Enemy " + enemy.name);
+
+				enemyToPlayerVector = enemy.transform.position - WeaponVector; // Enemy - Player
+
+				dotProduct = Vector2.Dot(AimVector, enemyToPlayerVector);
+				absoluteMagnitude = (Mathf.Sqrt(Mathf.Pow(AimVector.x, 2) + Mathf.Pow(AimVector.y, 2)) * Mathf.Sqrt(Mathf.Pow(enemyToPlayerVector.x, 2) + Mathf.Pow(enemyToPlayerVector.y, 2)));
+				angleBetween = Mathf.Acos(dotProduct / absoluteMagnitude) * Mathf.Rad2Deg;
+
+				// The reason I'm diving by 2 is because I'm thinking of MeleeRange as an FOV so if FOV is 30 they enemy can only be 15 degrees from the aim center.
+				if (angleBetween < MeleeRange / 2)
+				{
+					// Do damage
+					
+				}
+				Debug.Log("BFA: Angle between enemy and player aim is " + angleBetween);
+			}
+
+			StartCoroutine(Cooldown());
+		}
+	}
 
 	public void OnSecondary(InputAction.CallbackContext context)
 	{
-        Debug.Log("Secondary!");
+		
+		if (canAttack && context.started && CanWeSecondary())
+		{
+			// Debug
+			Debug.Log("BFA: Melee");
+			drawMeleeGizmos = true;
+
+			WeaponVector = transform.position;
+
+			// We get all the enemyies we have hit
+			enemyColliders = Physics2D.OverlapCircleAll(WeaponVector, MeleeReach, EnemyLayer);
+
+			foreach (Collider2D enemy in enemyColliders)
+			{
+				Debug.Log("BFA: Enemy " + enemy.name);
+
+				enemyToPlayerVector = enemy.transform.position - WeaponVector; // Enemy - Player
+
+				dotProduct = Vector2.Dot(AimVector, enemyToPlayerVector);
+				absoluteMagnitude = (Mathf.Sqrt(Mathf.Pow(AimVector.x, 2) + Mathf.Pow(AimVector.y, 2)) * Mathf.Sqrt(Mathf.Pow(enemyToPlayerVector.x, 2) + Mathf.Pow(enemyToPlayerVector.y, 2)));
+				angleBetween = Mathf.Acos(dotProduct / absoluteMagnitude) * Mathf.Rad2Deg;
+
+				// The reason I'm diving by 2 is because I'm thinking of MeleeRange as an FOV so if FOV is 30 they enemy can only be 15 degrees from the aim center.
+				if (angleBetween < MeleeRange / 2)
+				{
+					// Do damage
+
+				}
+				Debug.Log("BFA: Angle between enemy and player aim is " + angleBetween);
+			}
+
+			Data.RemoveCharge(Data.SelectedCharge);
+			StartCoroutine(Cooldown());
+		}
+	}
+	
+	private bool CanWeSecondary()
+	{
+		if(Data.SelectedCharge == ChargeType.FIRE)
+		{
+			Debug.Log("CanWeSecondary: " + Data.FireCharge);
+			return Data.FireCharge == 0 ? false : true;
+		}
+		else if (Data.SelectedCharge == ChargeType.ICE)
+		{
+			Debug.Log("CanWeSecondary: " + Data.IceCharge);
+			return Data.IceCharge == 0 ? false : true;
+		}
+		else if (Data.SelectedCharge == ChargeType.ELECTRIC)
+		{
+			Debug.Log("CanWeSecondary: " + Data.ElectricCharge);
+			return Data.ElectricCharge == 0 ? false : true;
+		}
+		else
+		{
+			Debug.LogError("CanWeSecondary");
+			return false;
+		}
+	}
+	
+	public void OnSpace(InputAction.CallbackContext context)
+	{
+		if(context.started)
+		{
+			
+		}
+	}
+	
+	public void OnWheel(InputAction.CallbackContext context)
+	{
+		if(context.performed)
+		{
+			if (context.ReadValue<Vector2>().y == 120f)
+			{
+				Data.CycleChargeType(1);
+				Debug.Log("SelectedCharge: " + Data.SelectedCharge);
+			}
+			else if (context.ReadValue<Vector2>().y == -120f)
+			{
+				Data.CycleChargeType(-1);
+				Debug.Log("SelectedCharge: " + Data.SelectedCharge);
+			}
+		}
 	}
 
 	public void OnControlsChanged()
 	{
-		// Nothing
+		// Nothing, just for the New Input System
+	}
+
+	private void OnDrawGizmos()
+	{
+		if (drawMeleeGizmos)
+		{
+			Gizmos.color = Color.red;
+			Gizmos.DrawRay(transform.position, AimVector); // Draws line from weapon to mouse cursor
+			Gizmos.DrawWireSphere(transform.position, MeleeReach); // Draws the reach of the melee weapon
+		}
+	}
+
+	private IEnumerator Cooldown()
+	{
+		canAttack = false;
+		yield return new WaitForSeconds(AttackCooldown);
+		canAttack = true;
 	}
 }
