8b742ee38d31eac6a2c7e8b54580371de5b05a5a
holypeachy <frankberm3@gmail.com>
2023-05-28 19:34:23 -0600

    Player can take damage. Damage calculation is done.

--- Assets/NPC/NPCStats.cs
+++ Assets/NPC/NPCStats.cs
@@ -4,34 +4,42 @@ using UnityEngine;
 
 public class NPCStats : MonoBehaviour
 {
-    public NPCStartingStat StartingStat;
+	public NPCStartingStat StartingStat;
 
-    public int health;
-    public int attack;
-    public int speed;
-    public int defense;
-    public float atkCoolDown;
-    public float iFrames;
-    public float deathTime;
-    public float range;
+	public int health;
+	
+	public int weakAttack;
+	public int normalAttack;
+	public int strongAttack;
+	
+	public int speed;
+	public int defense;
+	public float atkCoolDown;
+	public float iFrames;
+	public float deathTime;
+	public float range;
 
 
-    private void Awake()
-    {
-        health = StartingStat.initHealth;
-        attack = StartingStat.initAttack;
-        speed = StartingStat.initSpeed;
-        defense = StartingStat.initDefense;
-        atkCoolDown = StartingStat.initAtkCoolDown;
-        iFrames = StartingStat.initIFrames;
-        deathTime = StartingStat.initDeathTime;
-        range = StartingStat.initRange;
+	private void Awake()
+	{
+		health = StartingStat.initHealth;
+		
+		weakAttack = StartingStat.initWeakAttack;
+		normalAttack = StartingStat.initNormalAttack;
+		strongAttack = StartingStat.initStrongAttack;
+		
+		speed = StartingStat.initSpeed;
+		defense = StartingStat.initDefense;
+		atkCoolDown = StartingStat.initAtkCoolDown;
+		iFrames = StartingStat.initIFrames;
+		deathTime = StartingStat.initDeathTime;
+		range = StartingStat.initRange;
 
-    }
+	}
 
-    public void ResetAtkCoolDown()
-    {
-        atkCoolDown = StartingStat.initAtkCoolDown;
-    }
+	public void ResetAtkCoolDown()
+	{
+		atkCoolDown = StartingStat.initAtkCoolDown;
+	}
 
 }
