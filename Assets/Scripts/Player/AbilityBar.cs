6303398aebd3c26f502acaefeda24b5f79798ab2
Maitham Alghamgham <malghamgham@bellarmine.edu>
2023-05-28 23:04:41 -0400

    Added some feature for action bar for the player

--- Assets/Scripts/Player/AbilityBar.cs
+++ Assets/Scripts/Player/AbilityBar.cs
@@ -10,26 +10,42 @@ public class AbilityBar : MonoBehaviour
     public Button button2;
     public Button button3;
     public PlayerData playerData;
+
+    private Button selectedButton;
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             playerData.SelectedCharge = ChargeType.FIRE;
-            button.Select();
+            SelectButton(button);
             Debug.Log("The selected charge is: " + playerData.SelectedCharge);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             playerData.SelectedCharge = ChargeType.ICE;
-            button2.Select();
+            SelectButton(button2);
             Debug.Log("The selected charge is: " + playerData.SelectedCharge);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             playerData.SelectedCharge = ChargeType.ELECTRIC;
-            button3.Select();
+            SelectButton(button3);
             Debug.Log("The selected charge is: " + playerData.SelectedCharge);
         }
 
+    }
+
+    private void SelectButton(Button buttonToSelect)
+    {
+        if(selectedButton != null)
+        {
+            if (selectedButton == buttonToSelect)
+            {
+                return;
+            }
+           
+        }
+        selectedButton = buttonToSelect;
+        selectedButton.Select();         
     }
 }
